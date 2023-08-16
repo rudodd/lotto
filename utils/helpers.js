49163b5df16272bfa4b55a2a8d24bc54ee9d5769
Rustin Dodd <rustin.dodd@ibm.com>
2023-08-16 11:56:17 -0500

    initial setup and number class creation

---
+++ utils/helpers.js
@@ -0,0 +1,31 @@
+// Helper function that returns whether a carriable is empty, null, or undefined
+export const empty = (variable) => {
+
+  // check undefined 
+  if (typeof variable === 'undefined') {
+    return true;
+  
+  // check for null
+  } else if (variable === null) {
+    return true;
+
+  // check for empty string
+  } else if (typeof variable === 'string' && variable.length === 0){
+    return true;
+
+  //check for bad number  
+  } else if (typeof variable === 'number' && isNaN(variable)){
+    return true;
+
+  //check for empty object 
+  } else if (typeof variable === 'object' && Object.keys(variable).length === 0){
+    return true;
+
+  //check for empty arrays 
+  } else if (Array.isArray(variable) && variable.length === 0){
+    return true;
+
+  } else {
+    return false;
+  }
+}
---
+++ utils/numbers.js
@@ -0,0 +1,87 @@
+export default class Numbers {
+
+  // Class defaults
+  constructor () {
+    this.limit = 69;
+    this.break = Math.ceil(this.limit / 2);
+    this.odd = [];
+    this.even = [];
+    this.high = [];
+    this.low = [];
+    for (let i = 1; i <= this.limit; i++) {
+      if (i%2 > 0) {
+        this.odd.push(i);
+      } else {
+        this.even.push(i);
+      }
+
+      if (i <= this.break) {
+        this.low.push(i)
+      } else {
+        this.high.push(i);
+      }
+    }
+  }
+
+  // Random number generators
+  random() { return Math.floor(Math.random() * (this.limit - 1) + 1) };
+  powerBall() { return Math.floor(Math.random() * (26 - 1) + 1)  };
+
+  // Number checkers
+  isOdd(x) { return this.odd.includes(x) };
+  isEven(x) { return this.even.includes(x) };
+  isHigh(x) { return this.high.includes(x) };
+  isLow(x) { return this.low.includes(x) };
+
+  generateNumbers = (o,e,h,l) => {
+    const numbers = [];
+    let totalOdd = 0;
+    let totalEven = 0;
+    let totalHigh = 0;
+    let totalLow = 0;
+    for (let i = 0; numbers.length < 5 && i < 100000; i++) {
+      let num = this.random();
+      if (!numbers.includes(num)) {
+        if (totalOdd < o && this.isOdd(num)) {
+          if (totalHigh < h && this.isHigh(num)) {
+            numbers.push(num);
+            totalHigh++;
+          } else if (totalLow < l && this.isLow(num)) {
+            numbers.push(num);
+            totalLow++;
+          }
+          totalOdd++;
+        } else if (totalEven < e && this.isEven(num)) {
+          if (totalHigh < h && this.isHigh(num)) {
+            numbers.push(num);
+            totalHigh++;
+          } else if (totalLow < l && this.isLow(num)) {
+            numbers.push(num);
+            totalLow++;
+          }
+          totalEven++;
+        }
+      }
+    }
+    return numbers;
+  }
+
+  // Main play generator
+  generatePlay(numOdd, numEven, numHigh, numLow) {
+    if ((numOdd + numEven) === 5 && (numHigh + numLow) === 5) {
+      let play = [];
+      for (let i = 0; play.length === 0 && i < 1000; i++) {
+        const playAttempt = this.generateNumbers(numOdd, numEven, numHigh, numLow);
+        if (playAttempt.length === 5) {
+          play = playAttempt;
+        }
+        i++;
+      }
+      play.sort((a,b) => a - b);
+      play.push(this.powerBall());
+      return play;
+    } else {
+      throw 'The generate play method accepts only a combination 5 odd/even and 5 high/low';
+    }
+  }
+}
