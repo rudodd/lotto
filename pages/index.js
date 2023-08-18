13fe4b62f5ec917215c62957ef83b4e93e84a028
Rustin Dodd <rustin.dodd@ibm.com>
2023-08-18 15:38:16 -0500

    added skeleton mock loading state

--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,10 @@ import Numbers from '../utils/numbers';
 import { empty, titleCase } from '../utils/helpers';
 
 // import components
-import Head from 'next/head'
+import AppHead from '../components/AppHead';
+import AppSkeleton from '../components/AppSkeleton';
 import Button from '@mui/material/Button';
-import NumberCard from '../components/NumerCard';
+import NumberCard from '../components/NumberCard';
 import PlayGeneratorModal from '../components/PlayGeneratorModal';
 
 // Import icons
@@ -122,7 +123,9 @@ export default function Home() {
       !empty(hot) &&
       !empty(cold)
     ) {
-      setLoading(false);
+      setTimeout(() => {
+        setLoading(false);
+      }, 1000)
     }
   }, [prevResults, nextDrawing, lastDrawing, hot, cold])
 
@@ -138,31 +141,11 @@ export default function Home() {
 
   return (
     <>
-      {!loading &&
+      {loading ? (
+        <AppSkeleton />
+      ) : (
         <div>
-          <Head>
-            <title>Power Patterns</title>
-            <meta name="description" content="Powerball combinatorial number generator" />
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="apple-touch-icon" sizes="57x57" href="/img/icon/apple-icon-57x57.png" />
-            <link rel="apple-touch-icon" sizes="60x60" href="/img/icon/apple-icon-60x60.png" />
-            <link rel="apple-touch-icon" sizes="72x72" href="/img/icon/apple-icon-72x72.png" />
-            <link rel="apple-touch-icon" sizes="76x76" href="/img/icon/apple-icon-76x76.png" />
-            <link rel="apple-touch-icon" sizes="114x114" href="/img/icon/apple-icon-114x114.png" />
-            <link rel="apple-touch-icon" sizes="120x120" href="/img/icon/apple-icon-120x120.png" />
-            <link rel="apple-touch-icon" sizes="144x144" href="/img/icon/apple-icon-144x144.png" />
-            <link rel="apple-touch-icon" sizes="152x152" href="/img/icon/apple-icon-152x152.png" />
-            <link rel="apple-touch-icon" sizes="180x180" href="/img/icon/apple-icon-180x180.png" />
-            <link rel="icon" type="image/png" sizes="192x192"  href="/img/icon/android-icon-192x192.png" />
-            <link rel="icon" type="image/png" sizes="32x32" href="/img/icon/favicon-32x32.png" />
-            <link rel="icon" type="image/png" sizes="96x96" href="/img/icon/favicon-96x96.png" />
-            <link rel="icon" type="image/png" sizes="16x16" href="/img/icon/favicon-16x16.png" />
-            <link rel="manifest" href="/img/icon/manifest.json" />
-            <meta name="msapplication-TileColor" content="#ffffff" />
-            <meta name="msapplication-TileImage" content="/img/icon/ms-icon-144x144.png" />
-            <meta name="theme-color" content="#ffffff" />
-          </Head>
-
+          <AppHead />
           <main>
             <div className="logo">
               <h1><span>P</span><span>o</span><span>w</span><span>e</span><span>r</span> Patterns</h1>
@@ -178,7 +161,7 @@ export default function Home() {
             <div className="my-numbers">
               <div className="my-numbers-title-block">
                 <h3>My Numbers</h3>
-                {new Date(nextDrawing) > new Date(playDate) &&
+                {!empty(plays) && (new Date(nextDrawing) > new Date(playDate)) &&
                   <Button 
                     className="replay-button" 
                     variant="outlined" 
@@ -221,7 +204,7 @@ export default function Home() {
 
           </footer>
         </div>
-      }
+      )}
     </>
   )
 }
