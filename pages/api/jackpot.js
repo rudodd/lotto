cbf701c23b02cfc169cbf7424de2f852010e8ed2
Rustin Dodd <rustin.dodd@ibm.com>
2023-08-21 14:14:27 -0500

    added returns for the API responses

--- pages/api/jackpot.js
+++ pages/api/jackpot.js
@@ -6,16 +6,16 @@ export default function handler(req, res) {
   return fetch('https://www.powerball.com')
     .then((response) => {
       if (response.ok) {
-        response.text()
+        return response.text()
           .then((text) => {
             const $ = cheerio.load(text);
             $.html();
             const jackpot = $('.game-jackpot-number.text-xxxl').text();
             const cash = $('.game-jackpot-number.text-lg').text();
-            res.status(200).json({jackpot: jackpot, cash: cash});
+            return res.status(200).json({jackpot: jackpot, cash: cash});
           })
       } else {
-        res.status(response.status).json(response)
+        return res.status(response.status).json(response)
       }
     })
 }
