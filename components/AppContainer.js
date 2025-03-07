1e526dea8a9e1d7424464c01045f73386269b99b
Rustin Dodd <rustin.dodd@ibm.com>
2025-03-07 14:18:20 -0600

    adds google authentication

--- components/AppContainer.js
+++ components/AppContainer.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { signIn, signOut, useSession } from 'next-auth/react';
+
+// import components
 import AppHead from './AppHead';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import Avatar from '@mui/material/Avatar';
 
 export default function AppContainer(props) {
   const { title } = props;
+  const session = useSession();
+
+  useEffect(() => {
+    console.log(session);
+  }, [session])
 
   return (
     <div>
       <AppHead title={title} />
       <main>
-        <div className="logo">
-          <h1><span>P</span><span>o</span><span>w</span><span>e</span><span>r</span> Patterns</h1>
+        <div className="header">
+          <div className="logo">
+            <h1><span>P</span><span>o</span><span>w</span><span>e</span><span>r</span> Patterns</h1>
+          </div>
+          <div className="login">
+            {session?.status === 'authenticated' && session?.data?.user?.image ? (
+              <button onClick={() => signOut()} className="avatar">
+                <Avatar alt={session?.data?.user?.name} src={session?.data?.user?.image} sx={{ width: 30, height: 30 }} />
+              </button>
+            ) : (
+              <button onClick={() => signIn('google')} className="avatar">
+                <AccountCircleIcon sx={{ width: 30, height: 30 }} />
+              </button>
+            )}
+          </div>
         </div>
         {props.children}
       </main>
--- pages/_app.js
+++ pages/_app.js
@@ -5,13 +5,15 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+import { SessionProvider } from 'next-auth/react';
 
-function MyApp({ Component, pageProps }) {
+
+function MyApp({ Component, pageProps: {session, ...pageProps} }) {
   return (
-    <>
+    <SessionProvider session={session}>
       <CssBaseline />
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
     
   )
 }
