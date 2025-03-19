d039b0450070cf12bc7ac13d661b605cc9d2b477
Rustin Dodd <rustin.dodd@ibm.com>
2025-03-19 11:08:32 -0500

    refactors the numbers class

--- utils/numbers.js
+++ utils/numbers.js
@@ -32,7 +32,25 @@ export default class Numbers {
 
   // Random number generator helpers
   random() { return Math.floor(Math.random() * (this.limit - 1) + 1) };
+  randomHigh() { return Math.floor(Math.random() * (69 - 36 + 1) + 36)};
+  randomLow() { return Math.floor(Math.random() * (35 - 1 + 1) + 1)};
   powerBall() { return Math.floor(Math.random() * (26 - 1) + 1)  };
+  makeOdd(x) {
+    if (x % 2 === 0) {
+      const addOne = Math.random() < 0.5 ? true : false;
+      return addOne  ? x + 1 : x - 1;
+    } else {
+      return x;
+    }
+  };
+  makeEven(x) {
+    if (x % 2 !== 0) {
+      const addOne = Math.random() < 0.5 ? true : false;
+      return ((addOne && x < 70) || x < 3) ? x + 1 : x - 1;
+    } else {
+      return x;
+    }
+  };
 
   // Number checker helpers
   isOdd(x) { return x % 2 !== 0 };
@@ -54,44 +72,46 @@ export default class Numbers {
   // Number generator loop
   generateNumbers = (type, exclusions) => {
     const numbers = [];
-    if (type === null) {
-      for (let i = 0; numbers.length < 5 && i < 100000; i++) {
-        const excludedNumbers = this.getExcludedNumbers(exclusions, numbers);
-        let num = this.random();
-        if (!excludedNumbers.includes(num)) {
-          numbers.push(num);
-        }
+    let dominant = 0;
+    while (numbers.length < 5) {
+      const excludedNumbers = this.getExcludedNumbers(exclusions, numbers);
+      const isDominant = dominant < 3;
+      let num;
+      switch(type) {
+        case 'odd':
+          num = isDominant ? this.makeOdd(this.random()) : this.makeEven(this.random());
+          break;
+        case 'even':
+          num = isDominant ? this.makeEven(this.random()) : this.makeOdd(this.random());
+          break;
+        case 'high':
+          num = isDominant ? this.randomHigh() : this.randomLow();
+          break;
+        case 'low':
+          num = isDominant ? this.randomLow() : this.randomHigh();
+          break;
+        default:
+          num = this.random();
       }
-    } else {
-      let dominant = 0;
-      let others = 0;
-      for (let i = 0; numbers.length < 5 && i < 100000; i++) {
-        let num = this.random();
-        const excludedNumbers = this.getExcludedNumbers(exclusions, numbers);
-        if (!excludedNumbers.includes(num)) {
-          if (dominant < 3 && ((type === 'odd' && this.isOdd(num)) || (type === 'even' && this.isEven(num)) || (type === 'high' && this.isHigh(num)) || (type == 'low' && this.isLow(num)))) {
-            numbers.push(num);
-            dominant++;
-          } else if (others < 2 && ((type === 'odd' && !this.isOdd(num)) || (type === 'even' && !this.isEven(num)) || (type === 'high' && !this.isHigh(num)) || (type == 'low' && !this.isLow(num)))) {
-            numbers.push(num)
-            others++;
-          }
-        }
+      if (!excludedNumbers.includes(num)) {
+        numbers.push(num);
+        dominant++;
       }
     }
     return numbers;
   }
 
-  // Main play generator
+  // Main play generator (ensures plays sum in the correct range and adds powerball)
   generatePlay(type = null, exclusions = []) {
-    let play = [];
-    for (let i = 0; play.length === 0 && i < 1000; i++) {
+    let play;
+    let sumInRange = false;
+    while (!sumInRange) {
       const playAttempt = this.generateNumbers(type, exclusions);
       const sum = playAttempt.reduce((a, b) => a + b);
       if (playAttempt.length === 5 && (sum >= 130 && sum <= 221)) {
         play = playAttempt;
+        sumInRange = true;
       }
-      i++;
     }
     play.sort((a,b) => a - b);
     play.push(this.powerBall());
